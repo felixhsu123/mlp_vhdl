073be99f5f610f26453c87833b05e09336281ad8
nmarkko <50952339+nmarkko@users.noreply.github.com>
2020-01-23 18:01:40 +0100

    Update bram.vhd

--- mlp_sources/bram.vhd
+++ mlp_sources/bram.vhd
@@ -21,8 +21,9 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
+--use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 -- use IEEE.NUMERIC_STD.ALL;
@@ -36,6 +37,7 @@ entity bram is
     generic (WADDR: positive := 10;
     WDATA: positive := 16); 
     Port ( clka, clkb, ena, enb, wea, web : in STD_LOGIC;
+            reset: in STD_LOGIC;
            addra, addrb: in STD_LOGIC_VECTOR(WADDR-1 downto 0);
            dia, dib: in STD_LOGIC_VECTOR(WDATA-1 downto 0);
            doa, dob: out STD_LOGIC_VECTOR(WDATA-1 downto 0));
@@ -48,24 +50,32 @@ begin
 --Dual-Port logic port A
     process (clka) begin
         if clka'event and clka = '1' then
-            if ena = '1' then
-                if wea = '1' then
-                    mem (CONV_INTEGER (addra)) <= dia;
-                end if;
+            if (reset = '1') then
+                mem <= (others => (others => '0'));
             else
-                doa <= mem (CONV_INTEGER (addra));
+                if ena = '1' then
+                    if wea = '1' then
+                        mem (CONV_INTEGER (addra)) <= dia;
+                    end if;
+                else
+                    doa <= mem (CONV_INTEGER (addra));
+                end if;
             end if;
         end if;
     end process;
 --Dual-Port logic port B
     process (clkb) begin
         if clkb'event and clkb = '1' then
-            if enb = '1' then
-                if web = '1' then
-                    mem (CONV_INTEGER (addrb)) <= dib;
-                end if;
+            if (reset = '1') then
+                mem <= (others => (others => '0'));
             else
-                dob <= mem (CONV_INTEGER (addrb));
+                if enb = '1' then
+                    if web = '1' then
+                        mem (CONV_INTEGER (addrb)) <= dib;
+                    end if;
+                else
+                    dob <= mem (CONV_INTEGER (addrb));
+                end if;
             end if;
         end if;
     end process;
